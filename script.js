676e8456172aaec5f8ce551d66831f6aed95af20
Materia597 <sethj.browning@calbaptist.edu>
2025-09-16 20:23:39 -0700

    accept user input

--- script.js
+++ script.js
@@ -26,3 +26,116 @@ const initBoard = () => {
 }
 
 initBoard();
+
+/**
+ * 
+ * @param {string} pressedKey 
+ */
+const insertLetter = (pressedKey) => {
+    if (nextLetter === 5) return;
+
+    pressedKey = pressedKey.toLowerCase();
+
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    let box = row.children[nextLetter]
+    box.textContent = pressedKey;
+    box.classList.add("filled-box")
+    currentGuess.push(pressedKey)
+    nextLetter += 1
+}
+
+const deleteLetter = () => {
+    let row = document.getElementsByClassName('letter-row')[6 - guessesRemaining]
+    let guessString = ""
+    let rightGuess = Array.from(rightGuessString)
+
+    for (const val of currentGuess) {
+        guessString += val
+    }
+
+    if (guessString.length !== 5) {
+        alert("Not enough letters!")
+        return;
+    }
+
+    if (!WORDS.includes(guessString)) {
+        alert("Word not in list!")
+        return
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let letterColor = "";
+        let box = row.children[i];
+        let letter = currentGuess[i]
+
+        let letterPosition = rightGuess.indexOf(currentGuess[i])
+        if (letterPosition === -1) {
+            letterColor = 'grey'
+        } else {
+            if (currentGuess[i] === rightGuess[i]) {
+                letterColor = 'green'
+            } else {
+                letterColor = 'yellow'
+            }
+
+            rightGuess[letterPosition] = "#"
+        }
+
+        let delay = 250 * i;
+        setTimeout(() => {
+            box.style.backgroundColor = letterColor;
+            shadeKeyBoard(letter, letterColor);
+        }, delay)
+    }
+
+    if (guessString === rightGuessString) {
+        alert("You Guessed right! Game over!")
+        guessesRemaining = 0;
+        return;
+    } else {
+        guessesRemaining -= 1;
+        currentGuess = [];
+        nextLetter = 0;
+
+        if (guessesRemaining === 0) {
+            alert("You've run out of guesses! Game over!")
+            alert(`The right word was: ${rightGuessString}`)
+        }
+    }
+}
+
+const shadeKeyBoard = () => {
+    for (const elem of document.getElementsByClassName("keyboard-button")) {
+        if (elem.textContent === letter) {
+            let oldColor = elem.style.backgroundColor
+            if (oldColor === 'green') return;
+            if (oldColor === 'yellow' && color !== 'green') return;
+
+            elem.style.backgroundColor = color;
+            break;
+        }
+    }
+}
+
+document.addEventListener("keyup", (e) => {
+
+    if (guessesRemaining === 0) return;
+
+    let pressedKey = String(e.key)
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        deleteLetter()
+        return;
+    }
+
+    if (pressedKey === "Enter") {
+        checkGuess();
+        return;
+    }
+
+    let found = pressedKey.match(/[a-z]/gi)
+    if (!found || found.length > 1) {
+        return;
+    } else {
+        insertLetter(pressedKey)
+    }
+})
